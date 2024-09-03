b7edcfaab3a8680d87d1cb4547f78625455e49a8
ikimaka <ikimaka@yandex.ru>
2024-09-03 08:52:03 +0300

    Реализация RootComponent. Часть 1

--- app/src/main/java/com/ikimaka/whetherapp/presentation/root/RootComponent.kt
+++ app/src/main/java/com/ikimaka/whetherapp/presentation/root/RootComponent.kt
@@ -1,4 +1,22 @@
 package com.ikimaka.whetherapp.presentation.root
 
+import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.value.Value
+import com.ikimaka.whetherapp.presentation.details.DetailsComponent
+import com.ikimaka.whetherapp.presentation.favourite.FavouriteComponent
+import com.ikimaka.whetherapp.presentation.search.SearchComponent
+
 interface RootComponent {
+
+    val stack: Value<ChildStack<*, Child>>
+
+
+    sealed interface Child {
+
+        data class Favourite(val component: FavouriteComponent): Child
+
+        data class Search(val component: SearchComponent): Child
+
+        data class Details(val component: DetailsComponent): Child
+    }
 }
