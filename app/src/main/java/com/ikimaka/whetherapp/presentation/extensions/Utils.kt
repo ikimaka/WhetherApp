ffd596ed3376c0a35bc9784e72e35bc7d235ade5
ikimaka <ikimaka@yandex.ru>
2024-09-04 14:01:23 +0300

    FavouriteContent. Часть 2

--- app/src/main/java/com/ikimaka/whetherapp/presentation/extensions/Utils.kt
+++ app/src/main/java/com/ikimaka/whetherapp/presentation/extensions/Utils.kt
@@ -6,8 +6,11 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
+import kotlin.math.roundToInt
 
 fun ComponentContext.componentScope() = CoroutineScope(Dispatchers.Main + SupervisorJob())
     .apply {
         lifecycle.doOnDestroy { cancel() }
     }
+
+fun Float.tempToFormattedString(): String = "${roundToInt()}°C"
